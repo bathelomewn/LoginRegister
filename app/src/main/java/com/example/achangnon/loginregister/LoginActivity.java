d97f65a6ce4aa876559d17f62ae116e60c26ba0d
Tafadzwa Makuyana <tafadzwa.makuyana@students.stamford.edu>
2018-07-17 17:27:41 +0700

    refine the design

--- app/src/main/java/com/example/achangnon/loginregister/LoginActivity.java
+++ app/src/main/java/com/example/achangnon/loginregister/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.achangnon.loginregister;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void registerButton(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void login(View view) {
+        EditText username = (EditText)findViewById(R.id.etUsername);
+        EditText password = (EditText)findViewById(R.id.etPassword);
+
+        Intent intent = new Intent(this, UserAreaActivity.class);
+        startActivity(intent);
+
     }
 }
